dfd7a6c9c5c4757f9bea52eca0687e601ea5c736
Keito Fukuoka <keito.0o0.kirby@gmail.com>
2024-11-15 02:55:34 +0000

    feat(logger): add logger configuration setup
    Added a new logger configuration module using loguru. The setup_logger
    function initializes the logger with a specified name and log level,
    and writes logs to a file in the LOG_DIR directory. The logger is
    configured to write logs in UTF-8 encoding and supports backtrace.

---
+++ reshq/logger_config.py
@@ -0,0 +1,19 @@
+import loguru
+from aeug.paths import LOG_DIR
+
+
+def setup_logger(name: str, level: str = "INFO"):
+    # log_file = LOG_DIR / f"{{time}}_{name}.log"
+    log_file = LOG_DIR / f"{name}.log"
+    loguru.logger.add(
+        log_file,
+        backtrace=True,
+        level=level,
+        mode="w",
+        encoding="utf-8",
+    )
+
+    return loguru.logger
+
+
+logger = setup_logger("aeug")
