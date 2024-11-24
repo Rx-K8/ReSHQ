423ed9a606768b9bfa1b4dbaf8e4467efc3a14fe
Keito Fukuoka <keito.0o0.kirby@gmail.com>
2024-11-24 03:07:57 +0000

    feat(retrievers): add Splade retriever implementation
    - Added new file `splade_retriever.py` with Splade retriever class
    - Removed unused import `json` from `bm25_retriever.py`

---
+++ reshq/retrievers/splade_retriever.py
@@ -0,0 +1,41 @@
+from types import MappingProxyType
+
+from pyserini.search.lucene import LuceneImpactSearcher, LuceneSearcher
+
+from reshq.output_formats.search_result import SearchResults
+from reshq.result_converters.factory import converter_factory
+from reshq.retrievers.abc import Retriever
+from reshq.retrievers.bm25_retriever import bm25flat_index_mappings
+
+splade_index_mappings = MappingProxyType(
+    {
+        # TREC
+        "trec2019": "msmarco-v1-passage.splade-pp-ed",
+        "trec2020": "msmarco-v1-passage.splade-pp-ed",
+    }
+)
+
+
+class Splade(Retriever):
+    def __init__(self, benchmark: str) -> None:
+        self.name: str = "splade"
+        self.benchmark = benchmark
+        self.passage_type = splade_index_mappings[self.benchmark]
+        self.retriever = LuceneImpactSearcher.from_prebuilt_index(
+            self.passage_type,
+            "naver/splade-cocondenser-ensembledistil",
+        )
+        self.corpus = LuceneSearcher.from_prebuilt_index(
+            bm25flat_index_mappings[self.benchmark]
+        )
+        self.converter = converter_factory(benchmark)
+
+    def retrieve(self, query: str, top_k: int) -> SearchResults:
+        hits = self.retriever.search(query, top_k)
+        search_results = SearchResults()
+        for hit in hits:
+            doc_id = hit.docid
+            content = self.converter.convert(self.corpus.doc(doc_id).raw())
+            score = hit.score
+            search_results.add_search_result(query, doc_id, content, score)
+        return search_results
